86896d66bafda886482420de2a848ce63af7a802
Guilherme Augusto Rocha Sousa <guilhermesousadev@gmail.com>
2022-03-25 08:09:33 -0300

    Add files via upload

---
+++ GameOnline/frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { Container } from './style';
+
+const Home: FC = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+}
+
+export default Home;
