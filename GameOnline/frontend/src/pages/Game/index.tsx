f46171fc36e6c5169a49365b8682d70f108f3455
Guilherme Augusto Rocha Sousa <guilhermesousadev@gmail.com>
2022-04-03 11:44:13 -0300

    Add files via upload

--- GameOnline/frontend/src/pages/Game/index.tsx
+++ GameOnline/frontend/src/pages/Game/index.tsx
@@ -10,38 +10,91 @@ const Game: FC = () => {
   const { userId } = useContext(AuthContext);
 
   const [players, setPlayers] = useState<IPlayers[]>();
+  const [player, setPlayer] = useState<IPlayers>();
   const [keys] = useState<string[]>(["w", "a", "s", "d"]);
+
+  const updateUserPosition = useCallback(() => {}, []);
 
   const handleMove = useCallback(
     (key) => {
-      keys.map((k) => {
-        if (key === k) {
-          switch (key) {
-            case "w":
-              const player = players?.filter((p) => p.id === userId) as
-                | IPlayers
-                | undefined;
+      if (player) {
+        keys.map(async (k) => {
+          if (key === k) {
+            let obj = {
+              id: userId,
+              posLeft: player.posLeft,
+              posRight: player.posRight,
+              posX: player.posX,
+              posY: player.posY,
+            }
 
-              console.log(player);
-              break;
-            case "a":
-              console.log("a");
-              break;
-            case "s":
-              console.log("s");
-              break;
-            case "d":
-              console.log("d");
-              break;
-            default:
-              console.log("n");
-              break;
+            switch (key) {
+              case "w":
+                obj = {
+                  id: userId,
+                  posLeft: player.posLeft,
+                  posRight: player.posRight,
+                  posX: player.posX,
+                  posY: player.posY + 10,
+                }
+
+                await api.put('game', obj);
+                break;
+              case "a":
+                obj = {
+                  id: userId,
+                  posLeft: player.posLeft + 10,
+                  posRight: player.posRight,
+                  posX: player.posX,
+                  posY: player.posY,
+                }
+
+                await api.put('game', obj);
+                break;
+              case "s":
+                obj = {
+                  id: userId,
+                  posLeft: player.posLeft,
+                  posRight: player.posRight,
+                  posX: player.posX + 10,
+                  posY: player.posY,
+                }
+
+                await api.put('game', obj);
+                break;
+              case "d":
+                obj = {
+                  id: userId,
+                  posLeft: player.posLeft,
+                  posRight: player.posRight + 10,
+                  posX: player.posX,
+                  posY: player.posY,
+                }
+
+                await api.put('game', obj);
+                break;
+              default:
+                console.log("n");
+                break;
+            }
           }
-        }
-      });
+        });
+      }
     },
-    [keys, players, userId]
+    [keys, player, userId]
   );
+
+  const game = async () => {
+    if (players) {
+      const { data } = await api.get("game");
+
+      setPlayers(data);
+
+      setInterval(() => game, 1000);
+    }
+  }
+
+  game();
 
   useEffect(() => {
     window.addEventListener("keydown", (e) => handleMove(e.key));
@@ -52,8 +105,12 @@ const Game: FC = () => {
 
         setPlayers(data);
       }
+
+      if (players) {
+        setPlayer(players.filter((p) => p.id === userId)[0]);
+      }
     })();
-  }, [players, handleMove]);
+  }, [players, handleMove, userId, player]);
 
   return (
     <Container>
